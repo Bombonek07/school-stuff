cee0a4f778642408514baa62801b22d6245b4f45
Bombonek07 <ondrabondor07@gmail.com>
2023-10-26 20:06:47 +0200

    kocka funkcni

--- geometrie/tvary.py
+++ geometrie/tvary.py
@@ -1,3 +1,6 @@
+unit = ["mm", "cm", "dm", "m", "km"]
+shape = ["ctverec", "triangle", "rectangle", "square"]
+
 class jednotky:
     def __init__(self, j="cm"):
         self.__jednotky = j
@@ -6,12 +9,13 @@ class jednotky:
         self.__jednotky = j
         if self.__jednotky == "":
             self.__jednotky = "cm"
-            return "shape is not definite(set to default = cm)"
-        elif self.__jednotky != "mm"or"cm"or"dm"or"m"or"km":
+            return "unit is not definite(set to default = cm)"
+        elif self.__jednotky not in unit:
             self.__jednotky = "cm"
             return "trhis unit is not an option(set to default = cm)"
         else:
             self.__jednotky = j
+            return "unit set"
 
 class tvar:
     def __init__(self, t="ctverec"):
@@ -22,11 +26,20 @@ class tvar:
         if self.__tvar == "":
             self.__tvar = "ctverec"
             return "shape is not definite"
-        elif self.__tvar != "ctverec" or "triangle" or "rectangle" or "square":
+        elif self.__tvar not in shape:
             return "trhis shape is not an option"
         
         if self.__tvar == "ctverec":
-            pass
+            kocka = ctverec(int(input("déla strany 'a': ")))
+            what = str(input("obvod nebo obsah?:"))
+            if what == "obvod":
+                kocka.obvod()
+                return kocka.getobvod()
+            elif what == "obsah":
+                kocka.obsah()
+                return kocka.getobsah()
+            else:
+                return "nic"
 
 class ctverec:
     def __init__(self, a=0):
@@ -42,12 +55,11 @@ class ctverec:
         self.__obsah = self.__strana**2
 
     def getobvod(self):
-        return (self.__obvod)
+        return self.__obvod
 
     def getobsah(self):
         return (self.__obsah,"²")
         
-jednotky().setunit(str(input("jednotka ve které budete zadávat hodnoty\nvýběr(mm, cm, dm, m, km)\n")))
-tvar().setshape(str(input("tvar kderý chceš\nvýběr(ctverec, triangle, rectangle, square)\n")))
+print(jednotky().setunit(str(input("jednotka ve které budete zadávat hodnoty\nvýběr(mm, cm, dm, m, km)\n"))))
+print(tvar().setshape(str(input("tvar kderý chceš\nvýběr(ctverec, triangle, rectangle, square)\n"))))
 
-asdfghjklertzuierthjk
