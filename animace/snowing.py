f409c1f7294451d55e3e7d9afb670384bf8f28af
Bombonek07 <119570032+Bombonek07@users.noreply.github.com>
2023-11-28 10:13:30 +0100

    vlocky

--- animace/snowing.py
+++ animace/snowing.py
@@ -10,7 +10,6 @@ class Snowflake:
         self.game = game
         self.size = random.randint(3, 5)
         self.wind = random.randint(-1, 1)
-        self.color = random.randint(0, len(self.game.color)-1)
 
     def fall(self):
         self.y += self.size
@@ -23,20 +22,19 @@ class Snowflake:
             self.x = random.randint(0, self.game.width)
             self.size = random.randint(3, 5)
             self.wind = random.randint(-3, 1)
-            self.color = random.randint(0, len(self.game.color)-1)
 
     def draw(self):
-        pygame.draw.circle(self.game.screen, self.game.color[self.color], (self.x, self.y), self.size)
+        self.game.screen.blit(pygame.image.load("animace/snowflake.png"), (self.x, self.y))
 
 class game:
     def __init__(self):
         pygame.init()
         self.width, self.height = 1920, 1080
-        self.snowflakes_amount = 1200
-        self.color = self.make_colors()
+        self.snowflakes_amount = 100
+        self.img = pygame.image.load("animace/snowflake.png")
         self.snowflakes = self.make_snowflakes()
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("Snowfall Screensaver")
+        pygame.display.set_caption("Snowfall Screensaver"), (1,1)
         pygame.display.flip()
         self.game_loop()
 
@@ -46,12 +44,6 @@ class game:
             snowflakes.append(Snowflake(random.randint(0, self.width), random.randint(0, self.height), self))
         return snowflakes
 
-    def make_colors(self):
-        color = []
-        for _ in range(self.snowflakes_amount):
-            color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-        return color
-
     def game_loop(self):
         while True:
             for event in pygame.event.get():
@@ -59,15 +51,15 @@ class game:
                     pygame.quit()
                     sys.exit()
 
-            self.screen.fill((0 , 0, 0))
-            self.screen.blit(pygame.image.load("christmas_tree.jpg"), (0,0))
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(pygame.image.load("animace/christmas_tree.jpg"), (0,0))
 
             for snowflake in self.snowflakes:
                 snowflake.fall()
                 snowflake.draw()
 
             pygame.display.flip()
-            pygame.time.Clock().tick(100)
+            pygame.time.Clock().tick(120)
 
 if __name__ == "__main__":
     game()
