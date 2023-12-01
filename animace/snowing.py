1e6b1a08e65d056b7051217bd90cd9569c03552b
Bombonek07 <119570032+Bombonek07@users.noreply.github.com>
2023-12-01 09:20:07 +0100

    konecne epileptak

--- animace/snowing.py
+++ animace/snowing.py
@@ -9,15 +9,20 @@ class Snowflake:
         self.x = x
         self.y = y
         self.game = game
-        self.size = random.randint(5, 60)
-        self.random = random.randint(1, 500)
         self.img = pygame.image.load(random.choice(self.vlocka))
+
+        self.random = random.randint(1, 500)
         if self.random == 1:
-            self.size = 300
-            self.img = pygame.image.load(random.choice(self.vlocka))
-        elif self.random == 2:
             self.size = 200
-            self.img = pygame.image.load(random.choice(self.vlocka))
+        elif self.random == 2:
+            self.size = 150
+        elif self.random == (3 or 4 or 5):
+            self.size = 125
+        elif self.random == (6 or 7 or 8):
+            self.size = 100
+        else:
+            self.size = random.randint(5, 60)
+        
         self.wind = random.randint(-1, 1)
         if self.size < 30:
             self.img = pygame.image.load("animace/snowflake_blor.png")
@@ -26,13 +31,20 @@ class Snowflake:
         self.rotate_img = pygame.transform.rotate(self.img, self.deg)
 
     def fall(self):
+        self.deg2 = random.randint(-1,1)
+        self.rotate_img = pygame.transform.rotate(self.img, (self.deg+self.deg2))
+
         if self.size < 20:
             self.y += self.size / 20
-        if self.size < 40:
+        elif self.size < 40:
             self.y += self.size / 15
-        if self.size >= 40:
+        elif self.size >= 40:
+            self.y += self.size / 11
+        elif self.size >= 50:
+            self.y += self.size / 10.5
+        elif self.size >= 100:
             self.y += self.size / 10
-        if self.size >= 60:
+        elif self.size >= 200:
             self.y += self.size / 9.9
         if self.wind == -1:
             self.x += math.sin(self.y/90)
@@ -40,9 +52,26 @@ class Snowflake:
             self.x += math.cos(self.y/90)          
         if self.y > self.game.height:
             self.y = -200
-            self.x = random.randint(-200, self.game.width+200)
-            self.deg = random.randint(0, 90)
+            self.x = random.randint(-100, self.game.width+100)
             self.wind = random.randint(-1, 1)
+            
+            self.random = random.randint(1, 500)
+            if self.random == 1:
+                self.size = 200
+            elif self.random == 2:
+                self.size = 150
+            elif self.random == (3 or 4 or 5):
+                self.size = 125
+            elif self.random == (6 or 7 or 8):
+                self.size = 100
+            else:
+                self.size = random.randint(5, 60)
+            
+            if self.size < 30:
+                self.img = pygame.image.load("animace/snowflake_blor.png")
+            else:
+                self.img = pygame.image.load(random.choice(self.vlocka))
+            self.img = pygame.transform.scale(self.img, (self.size, self.size))
 
     def draw(self):
         self.game.screen.blit(self.rotate_img, (self.x, self.y))
@@ -51,7 +80,7 @@ class game:
     def __init__(self):
         pygame.init()
         self.width, self.height = 1920, 1080
-        self.snowflakes_amount = 250
+        self.snowflakes_amount = 300
         self.snowflakes = self.make_snowflakes()
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Snowfall Screensaver"), (1,1)
@@ -79,7 +108,7 @@ class game:
                 snowflake.draw()
 
             pygame.display.flip()
-            pygame.time.Clock().tick(9999999999)
+            pygame.time.Clock().tick(50)
 
 if __name__ == "__main__":
     game()
